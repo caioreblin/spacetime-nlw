2cc53a2e8bc2d9bfce23ff33acb92884cc6dcf52
Caio Henrique Reblin <caiohr@weg.net>
2024-01-20 22:36:22 -0300

    Added server files

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/hello', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log(`HTTP server running on http://localhost:3333`)
+  })
+  .catch((err) => {
+    console.error(`Error starting server: ${err}`)
+  })
